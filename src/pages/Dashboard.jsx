cec6571a74007290afa172bf10e7309e2ae3e0ae
Ben <ben.a.burgos@gmail.com>
2022-05-14 16:41:09 -0700

    adds table to dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,19 +1,27 @@
-import stocks from '../data/data'
+import stocks from '../data/data';
 
 export default function Dashboard() {
-    return(
-        <div className='dashboard-table'>
-           <h2>Most Active Stocks</h2>
-           <table>
-               <tr>
-                   <th>Company Name (Symbol)</th>
-                   <th>Price</th>
-                   <th>Change</th>
-               </tr>
-               <tr><td>test</td><td>test</td><td>test</td></tr>
-               <tr><td>test</td><td>test</td><td>test</td></tr>
-               <tr><td>test</td><td>test</td><td>test</td></tr>
-            </table> 
-        </div>
-    )
+  return (
+    <div className="dashboard-table">
+      <h2>Most Active Stocks</h2>
+      <table>
+        <tr>
+          <th>Company Name (Symbol)</th>
+          <th>Price</th>
+          <th>Change</th>
+        </tr>
+        {stocks.map(({ name, symbol, lastPrice, change, open }) => {
+          return (
+            <tr key={symbol}>
+              <td>{`${name} (${symbol})`}</td>
+              <td>{lastPrice}</td>
+              <td className={Math.sign(change) === -1 ? 'change-red':'change-green'}>
+                {`${(lastPrice - open).toFixed(2)} (${change.toFixed(2)}%)`}
+              </td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
