91ada1679f6d1037be78c06586e56a26d4d2e6a3
Ben <ben.a.burgos@gmail.com>
2022-05-14 15:12:46 -0700

    adds boilerplate

---
+++ src/pages/About.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+    return <h1>You're at the About page!</h1>
+}
---
+++ src/pages/Stock.jsx
@@ -0,0 +1,3 @@
+export default function Stock() {
+    return <h1>You're at the Stock page!</h1>
+}
