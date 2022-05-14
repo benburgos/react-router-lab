8a200e8cb4d23e4bd2c692d26bf697664e70604b
Ben <ben.a.burgos@gmail.com>
2022-05-14 15:17:14 -0700

    adds routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Stock from './pages/Stock';
+import Dashboard from './pages/Dashboard';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/stocks/:symbol" element={<Stock />} />
+        <Route path="/stocks" element={<Dashboard />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
