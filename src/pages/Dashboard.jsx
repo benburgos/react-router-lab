00407730511a9b77b1bfd84a8489a801586e889d
Ben <ben.a.burgos@gmail.com>
2022-05-14 16:04:22 -0700

    adds table and initial style

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,19 @@
+import stocks from '../data/data'
+
 export default function Dashboard() {
-    return <h1>You're at the Dashboard page!</h1>
+    return(
+        <div className='dashboard-table'>
+           <h2>Most Active Stocks</h2>
+           <table>
+               <tr>
+                   <th>Company Name (Symbol)</th>
+                   <th>Price</th>
+                   <th>Change</th>
+               </tr>
+               <tr><td>test</td><td>test</td><td>test</td></tr>
+               <tr><td>test</td><td>test</td><td>test</td></tr>
+               <tr><td>test</td><td>test</td><td>test</td></tr>
+            </table> 
+        </div>
+    )
 }
