db49770f42cf9f248cefb428ebbb0194cc790272
Ben <ben.a.burgos@gmail.com>
2022-05-14 17:09:26 -0700

    makes logo more obvious

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ export default function Nav() {
   return (
     <div className="navbar">
       <Link to={'/'}>
-        <div>stockRouter</div>
+        <div className='logo'>stockRouter.js</div>
       </Link>
       <Link to={'/stocks'}>
         <div className='nav-link'>Dashboard</div>
