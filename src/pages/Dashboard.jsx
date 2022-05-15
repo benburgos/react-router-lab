ece94dbd14c3d7c41435579a6399d9a770eb3fdc
Ben <ben.a.burgos@gmail.com>
2022-05-14 17:07:52 -0700

    adds links and finishes table

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import stocks from '../data/data';
+import { Link } from 'react-router-dom';
 
 export default function Dashboard() {
   return (
@@ -10,13 +11,19 @@ export default function Dashboard() {
           <th>Price</th>
           <th>Change</th>
         </tr>
-        {stocks.map(({ name, symbol, lastPrice, change, open }) => {
+        {stocks.map(({ name, symbol, lastPrice, change, open }, index) => {
           return (
-            <tr key={symbol}>
-              <td>{`${name} (${symbol})`}</td>
+            <tr key={index}>
+              <td>
+                <Link to={`/stocks/${symbol}`}>{`${name} (${symbol})`}</Link>
+              </td>
+
               <td>{lastPrice}</td>
-              <td className={Math.sign(change) === -1 ? 'change-red':'change-green'}>
-                {`${(lastPrice - open).toFixed(2)} (${change.toFixed(2)}%)`}
+              <td className={Math.sign(change) === -1 ? 'red' : 'green'}>
+                {`
+                ${Math.sign(change) === 1 ? '+' : '-'} 
+                ${Math.abs(open - lastPrice).toFixed(2)} (${change.toFixed(2)}%)
+                `}
               </td>
             </tr>
           );
